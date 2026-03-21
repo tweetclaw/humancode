c08103994e6e4ba3c676ad749b7baa0ac0a47106
tweetclaw <tweetclaw@users.noreply.github.com>
2026-03-21 12:09:29 +0800

    ignore and log level

--- startcode.sh
+++ startcode.sh
@@ -7,4 +7,4 @@ cd "$(dirname "$0")"
 env -i HOME="$HOME" USER="$USER" SHELL="$SHELL" TMPDIR="$TMPDIR" \
 PATH="/usr/local/opt/node@22/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin" \
 LANG="${LANG:-en_US.UTF-8}" \
-./scripts/code.sh --user-data-dir /tmp/vscode-dev-user-data --extensions-dir /tmp/vscode-dev-extensions
+./scripts/code.sh --log error --user-data-dir /tmp/vscode-dev-user-data --extensions-dir /tmp/vscode-dev-extensions 2>&1 | grep --line-buffered -vE "ResizeObserver|geminicodeassist|client not ready|DEBUGGING:|LoadCodeAssistResponse|service/healthcheck"
